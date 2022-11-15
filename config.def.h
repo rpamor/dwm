97aba30c5d74992ce010e07fbc00a906200694d2
Richard Pamor <richard.pamor@yahoo.com>
2022-11-15 15:30:28 +0800

    feat: update rules array

--- config.def.h
+++ config.def.h
@@ -57,7 +57,6 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class                instance                title   tags mask   isfloating  monitor */
-  { "KeePassXC",          NULL,                   NULL,   0,          1,          -1 },
   { "Seahorse",           NULL,                   NULL,   0,          1,          -1 },
   { "firefox",            NULL,                   NULL,   1 << 2,     0,          -1 },
   { "qutebrowser",        NULL,                   NULL,   1 << 2,     0,          -1 },
